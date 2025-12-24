173bf71325c9f8a8f965fd48cb8812bee04387d1
Andrew_CS77 <166889684+CahSragen77@users.noreply.github.com>
2025-12-24 21:41:26 +0700

    Create datetimes.js

---
+++ JS/datetimes.js
@@ -0,0 +1,16 @@
+function updateTime() {
+    const now = new Date();
+    const options = { 
+        weekday: 'long', 
+        year: 'numeric', 
+        month: 'long', 
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false 
+    };
+    document.getElementById('liveTime').textContent = now.toLocaleDateString('id-ID', options);
+}
+setInterval(updateTime, 1000);
+updateTime();
